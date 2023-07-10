ad01553e54f9a314401b778dcfed1f9b9f5e2779
Umarbrainee <99734694+Umarbrainee@users.noreply.github.com>
2023-07-10 14:35:24 +0000

    commit

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello World")
+cities_to_check = ['lahore','islamabad']  
+clean_city = str(input(''))
+clean_city = clean_city.lower()
+for check_city in cities_to_check:
+    if clean_city == check_city:
+        print("List of clean cities")
+        break
+    else:
+        print("Not a clean city")
+        break
